b9689531b21ef3acd25580dcf16bf2c4a0bb700d
iamjanolaerts <jan.olaerts92@gmail.com>
2019-10-15 11:48:26 -0500

    Array solved

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { NavContext } from '../contexts/NavContext';
+import uuid from 'uuid/v1'
 
 const Favorites = () => {
     let { toggleNav } = useContext(NavContext);
@@ -11,8 +12,20 @@ const Favorites = () => {
     return (
         <div className="favorites" >
             <img className="menu" src={require("../img/tacomenu.png")} onClick={toggleNav} />
-            <div className="favorites-title">YOUR FAVORITES</div>
-            <div>{ favorites }</div>
+            <div className="favorites-title">YOUR ORDER HISTORY</div>
+
+            <div className="favorites-list">
+            { Array.isArray(favoritesArray) && favoritesArray.map(favorite => {
+            return  <div className="favorite" key={uuid()}>    
+                        <div>{ favorite.baseLayer }</div>
+                        <div>{ favorite.condiment }</div>
+                        <div>{ favorite.mixing }</div>
+                        <div>{ favorite.seasoning }</div>
+                        <div>{ favorite.Shell }</div>
+                    </div>    
+            })}
+            </div>
+
         </div>
     );
 }
